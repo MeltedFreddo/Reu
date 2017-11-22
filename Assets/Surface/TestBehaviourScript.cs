c3d7a5d7e0092e260b33ddcf59f86ef8b0ce2708
Craig Edwards <craig@setmetrics.com>
2017-11-22 11:29:19 +1100

    Added scene SurfaceView with dummy content

---
+++ Assets/Surface/TestBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{		
+			var hitInfo = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+
+			if (hitInfo.transform != null && hitInfo.transform.gameObject == this.gameObject) 
+			{
+				Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+
+				//SceneManager.UnloadSceneAsync("SystemView");
+				var systemViewScene = SceneManager.GetSceneByName("SystemView");
+				SceneManager.SetActiveScene(systemViewScene);
+
+			} else {
+
+			}
+		}
+	}
+}
