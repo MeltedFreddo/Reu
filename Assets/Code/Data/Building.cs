84dc425f89654572a58e0b3ea4acf544f816f2b1
Craig Edwards <cdedw1@gmail.com>
2017-12-14 00:15:34 +1100

    Made a building attribute class

--- Assets/Code/Data/Building.cs
+++ Assets/Code/Data/Building.cs
@@ -1,12 +1,12 @@
-﻿using Assets.Code.Data.Lists;
-
-namespace Assets.Code.Data
+﻿namespace Assets.Code.Data
 {
 	public class Building
 	{
 		public string BuildingType { get; set; }
 
-		public int PowerConsumption { get; set; }
+        public decimal Cost { get; set; }
+
+        public int PowerConsumption { get; set; }
 
 		public bool IsActive { get; set; }
 
---
+++ Assets/Code/Data/GameContent/Buildings.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections.Generic;
+using Assets.Code.Data.Lists;
+
+namespace Assets.Code.Data.GameContent
+{
+    public static class Buildings
+    {
+        public static Building Administration
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.Administration,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 4,
+                    WidthInTiles = 4
+                };
+            }
+        }
+        public static Building Derrick
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.Derrick,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 2,
+                    WidthInTiles = 3
+                };
+            }
+        }
+        public static Building Farm
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.Farm,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 2,
+                    WidthInTiles = 2
+                };
+            }
+        }
+        public static Building Hospital
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.Hospital,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 4,
+                    WidthInTiles = 4
+                };
+            }
+        }
+        public static Building Mine
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.Mine,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 3,
+                    WidthInTiles = 3
+                };
+            }
+        }
+        public static Building MiningStation
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.MiningStation,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 4,
+                    WidthInTiles = 4
+                };
+            }
+        }
+        public static Building Observatory
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.Observatory,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 3,
+                    WidthInTiles = 3
+                };
+            }
+        }
+        public static Building Park
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.Park,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 2,
+                    WidthInTiles = 2
+                };
+            }
+        }
+        public static Building PowerPlant
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.PowerPlant,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 4,
+                    WidthInTiles = 4
+                };
+            }
+        }
+        public static Building Residence
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.Residence,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 2,
+                    WidthInTiles = 2
+                };
+            }
+        }
+        public static Building Spaceport
+        {
+            get
+            {
+                return new Building
+                {
+                    BuildingType = BuildingType.Spaceport,
+                    Cost = 10,
+                    IsActive = true,
+                    PowerConsumption = 1,
+                    HeightInTiles = 4,
+                    WidthInTiles = 4
+                };
+            }
+        }
+
+        public static IEnumerable<Building> GetAllBuildings()
+        {
+            return new List<Building>
+            {
+                Administration,
+                Derrick,
+                Farm,
+                Hospital,
+                Mine,
+                MiningStation,
+                Observatory,
+                Park,
+                PowerPlant,
+                Residence,
+                Spaceport
+            };
+        }
+    }
+}
